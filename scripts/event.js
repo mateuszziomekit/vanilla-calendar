a9e5c3f5ecee5a93bdf0dcac336cdac5dc1d27fe
Mateusz Ziomek <mateusz@mateuszziomekit.com>
2024-09-11 09:39:24 +0200

    part 20: rendering week calendar events

--- scripts/event.js
+++ scripts/event.js
@@ -1,11 +1,32 @@
 const eventTemplateElement = document.querySelector("[data-template='event']");
 
+const dateFormatter = new Intl.DateTimeFormat("en-US", {
+  hour: "numeric",
+  minute: "numeric"
+});
+
 export function initStaticEvent(parent, event) {
   const eventElement = initEvent(event);
 
   if (isEventAllDay(event)) {
     eventElement.classList.add("event--filled");
   }
+
+  parent.appendChild(eventElement);
+}
+
+export function initDynamicEvent(parent, event, dynamicStyles) {
+  const eventElement = initEvent(event);
+
+  eventElement.classList.add("event--filled");
+  eventElement.classList.add("event--dynamic");
+
+  eventElement.style.top = dynamicStyles.top;
+  eventElement.style.left = dynamicStyles.left;
+  eventElement.style.bottom = dynamicStyles.bottom;
+  eventElement.style.right = dynamicStyles.right;
+
+  eventElement.dataset.eventDynamic = true;
 
   parent.appendChild(eventElement);
 }
@@ -17,8 +38,13 @@ function initEvent(event) {
   const eventStartTimeElement = eventElement.querySelector("[data-event-start-time]");
   const eventEndTimeElement = eventElement.querySelector("[data-event-end-time]");
 
+  const startDate = eventTimeToDate(event, event.startTime);
+  const endDate = eventTimeToDate(event, event.endTime);
+
   eventElement.style.setProperty("--event-color", event.color);
   eventTitleElement.textContent = event.title;
+  eventStartTimeElement.textContent = dateFormatter.format(startDate);
+  eventEndTimeElement.textContent = dateFormatter.format(endDate);
 
   return eventElement;
 }
@@ -31,10 +57,40 @@ export function eventStartsBefore(eventA, eventB) {
   return eventA.startTime < eventB.startTime;
 }
 
+export function eventEndsBefore(eventA, eventB) {
+  return eventA.endTime < eventB.eventTime;
+}
+
+export function eventCollidesWith(eventA, eventB) {
+  const maxStartTime = Math.max(eventA.startTime, eventB.startTime);
+  const minEndTime = Math.min(eventA.endTime, eventB.endTime);
+
+  return minEndTime > maxStartTime;
+}
+
+export function eventTimeToDate(event, eventTime) {
+  return new Date(
+    event.date.getFullYear(),
+    event.date.getMonth(),
+    event.date.getDate(),
+    0,
+    eventTime
+  );
+}
+
 export function validateEvent(event) {
   if (event.startTime >= event.endTime) {
     return "Event end time must be after start time";
   }
 
   return null;
+}
+
+export function adjustDynamicEventMaxLines(dynamicEventElement) {
+  const availableHeight = dynamicEventElement.offsetHeight;
+  const lineHeight = 16;
+  const padding = 8;
+  const maxTitleLines = Math.floor((availableHeight - lineHeight - padding) / lineHeight);
+
+  dynamicEventElement.style.setProperty("--event-title-max-lines", maxTitleLines);
 }
--- scripts/week-calendar.js
+++ scripts/week-calendar.js
@@ -1,5 +1,5 @@
 import { generateWeekDays, isTheSameDay, today } from "./date.js";
-import { isEventAllDay } from "./event.js";
+import { isEventAllDay, eventStartsBefore, eventEndsBefore, initDynamicEvent, eventCollidesWith, adjustDynamicEventMaxLines } from "./event.js";
 import { initEventList } from "./event-list.js";
 
 const calendarTemplateElement = document.querySelector("[data-template='week-calendar']");
@@ -22,10 +22,13 @@ export function initWeekCalendar(parent, selectedDate, eventStore, isSingleDay) 
   for (const weekDay of weekDays) {
     const events = eventStore.getEventsByDate(weekDay);
     const allDayEvents = events.filter((event) => isEventAllDay(event));
+    const nonAllDayEvents = events.filter((event) => !isEventAllDay(event));
+
+    sortEventsByTime(nonAllDayEvents);
 
     initDayOfWeek(calendarDayOfWeekListElement, selectedDate, weekDay);
     initAllDayListItem(calendarAllDayListElement, allDayEvents);
-    initColumn(calendarColumnsElement, weekDay);
+    initColumn(calendarColumnsElement, weekDay, events);
   }
 
   if (isSingleDay) {
@@ -33,6 +36,12 @@ export function initWeekCalendar(parent, selectedDate, eventStore, isSingleDay) 
   }
 
   parent.appendChild(calendarElement);
+
+  const dynamicEventElements = calendarElement.querySelectorAll("[data-event-dynamic]");
+
+  for (const dynamicEventElement of dynamicEventElements) {
+    adjustDynamicEventMaxLines(dynamicEventElement);
+  }
 }
 
 function initDayOfWeek(parent, selectedDate, weekDay) {
@@ -61,10 +70,187 @@ function initAllDayListItem(parent, events) {
   parent.appendChild(calendarAllDayListItemElement);
 }
 
-function initColumn(parent, weekDay) {
+function initColumn(parent, weekDay, events) {
   const calendarColumnContent = calendarColumnTemplateElement.content.cloneNode(true);
   const calendarColumnElement = calendarColumnContent.querySelector("[data-week-calendar-column]");
   const calendarColumnCellElements = calendarColumnElement.querySelectorAll("[data-week-calendar-cell]");
 
+  const eventsWithDynamicStyles = calculateEventsDynamicStyles(events);
+  for (const eventWithDynamicStyles of eventsWithDynamicStyles) {
+    initDynamicEvent(
+      calendarColumnElement,
+      eventWithDynamicStyles.event,
+      eventWithDynamicStyles.styles
+    );
+  }
+
   parent.appendChild(calendarColumnElement);
+}
+
+function calculateEventsDynamicStyles(events) {
+  const { eventGroups, totalColumns } = groupEvents(events);
+  console.log(eventGroups);
+  const columnWidth = 100 / totalColumns;
+  const initialEventGroupItems = [];
+
+  for (const eventGroup of eventGroups) {
+    for (const eventGroupItem of eventGroup) {
+      if (eventGroupItem.isInitial) {
+        initialEventGroupItems.push(eventGroupItem);
+      }
+    }
+  }
+
+  return initialEventGroupItems.map((eventGroupItem) => {
+    const topPercentage = 100 * (eventGroupItem.event.startTime / 1440);
+    const bottomPercentage = 100 - 100 * (eventGroupItem.event.endTime / 1440);
+    const leftPercentage = columnWidth * eventGroupItem.columnIndex;
+    const rightPercentage = columnWidth * (totalColumns - eventGroupItem.columnIndex - eventGroupItem.columnSpan);
+
+    return {
+      event: eventGroupItem.event,
+      styles: {
+        top: `${topPercentage}%`,
+        bottom: `${bottomPercentage}%`,
+        left: `${leftPercentage}%`,
+        right: `${rightPercentage}%`
+      }
+    }
+  });
+}
+
+function groupEvents(events) {
+  if (events.length === 0) {
+    return { eventGroups: [], totalColumns: 0 };
+  }
+
+  const firstEventGroup = [
+    {
+      event: events[0],
+      columnIndex: 0,
+      isInitial: true,
+      eventIndex: 0
+    }
+  ];
+
+  const eventGroups = [firstEventGroup];
+
+  for (let i = 1; i < events.length; i += 1) {
+    const lastEventGroup = eventGroups[eventGroups.length - 1];
+    const loopEvent = events[i];
+
+    const lastEventGroupCollidingItems = lastEventGroup.filter((eventGroupItem) => eventCollidesWith(eventGroupItem.event, loopEvent));
+
+    if (lastEventGroupCollidingItems.length === 0) {
+      const newEventGroupItem = {
+        event: loopEvent,
+        columnIndex: 0,
+        isInitial: true,
+        eventIndex: i
+      };
+
+      const newEventGroup = [newEventGroupItem];
+      eventGroups.push(newEventGroup);
+      continue;
+    }
+
+    if (lastEventGroupCollidingItems.length === lastEventGroup.length) {
+      const newEventGroupItem = {
+        event: loopEvent,
+        columnIndex: lastEventGroup.length,
+        isInitial: true,
+        eventIndex: i
+      };
+
+      lastEventGroup.push(newEventGroupItem);
+      continue;
+    }
+
+    let newColumnIndex = 0;
+    while (true) {
+      const isColumnIndexInUse = lastEventGroupCollidingItems.some((eventGroupItem) => eventGroupItem.columnIndex === newColumnIndex);
+
+      if (isColumnIndexInUse) {
+        newColumnIndex += 1;
+      } else {
+        break;
+      }
+    }
+
+    const newEventGroupItem = {
+      event: loopEvent,
+      columnIndex: newColumnIndex,
+      isInitial: true,
+      eventIndex: i
+    };
+
+    const newEventGroup = [
+      ...lastEventGroupCollidingItems.map((eventGroupItem) => ({
+        ...eventGroupItem,
+        isInitial: false
+      })),
+      newEventGroupItem
+    ];
+
+    eventGroups.push(newEventGroup);
+  }
+
+  let totalColumns = 0;
+  for (const eventGroup of eventGroups) {
+    for (const eventGroupItem of eventGroup) {
+      totalColumns = Math.max(totalColumns, eventGroupItem.columnIndex + 1);
+    }
+  }
+
+  for (const eventGroup of eventGroups) {
+    eventGroup.sort((columnGroupItemA, columnGroupItemB) => {
+      return columnGroupItemA.columnIndex < columnGroupItemB.columnIndex ? -1 : 1;
+    });
+
+    for (let i = 0; i < eventGroup.length; i += 1) {
+      const loopEventGroupItem = eventGroup[i];
+      if (i === eventGroup.length - 1) {
+        loopEventGroupItem.columnSpan = totalColumns - loopEventGroupItem.columnIndex;
+      } else {
+        const nextLoopEventGroupItem = eventGroup[i + 1];
+        loopEventGroupItem.columnSpan = nextLoopEventGroupItem.columnIndex - loopEventGroupItem.columnIndex;
+      }
+    }
+  }
+
+  for (let i = 0; i < events.length; i += 1) {
+    let lowestColumnSpan = Infinity;
+
+    for (const eventGroup of eventGroups) {
+      for (const eventGroupItem of eventGroup) {
+        if (eventGroupItem.eventIndex === i) {
+          lowestColumnSpan = Math.min(lowestColumnSpan, eventGroupItem.columnSpan);
+        }
+      }
+    }
+
+    for (const eventGroup of eventGroups) {
+      for (const eventGroupItem of eventGroup) {
+        if (eventGroupItem.eventIndex === i) {
+          eventGroupItem.columnSpan = lowestColumnSpan;
+        }
+      }
+    }
+  }
+
+  return { eventGroups, totalColumns };
+}
+
+function sortEventsByTime(events) {
+  events.sort((eventA, eventB) => {
+    if (eventStartsBefore(eventA, eventB)) {
+      return -1;
+    }
+
+    if (eventStartsBefore(eventB, eventA)) {
+      return 1
+    }
+
+    return eventEndsBefore(eventA, eventB) ? 1 : -1;
+  });
 }
