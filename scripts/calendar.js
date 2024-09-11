288f26ca7c3897730af84fcdfa09d86cf38872c0
Mateusz Ziomek <mateusz@mateuszziomekit.com>
2024-09-11 09:39:22 +0200

    part 8: day calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,6 +1,7 @@
 export function initCalendar() {
   const monthCalendarElement = document.querySelector("[data-month-calendar]");
   const weekCalendarElement = document.querySelector("[data-week-calendar]");
+  const dayCalendarElement = document.querySelector("[data-day-calendar]");
 
   document.addEventListener("view-change", (event) => {
     const selectedView = event.detail.view;
@@ -8,12 +9,15 @@ export function initCalendar() {
     if (selectedView === "month") {
       monthCalendarElement.style.display = "flex";
       weekCalendarElement.style.display = "none";
+      dayCalendarElement.style.display = "none";
     } else if (selectedView === "week") {
       monthCalendarElement.style.display = "none";
       weekCalendarElement.style.display = "flex";
+      dayCalendarElement.style.display = "none";
     } else {
       monthCalendarElement.style.display = "none";
       weekCalendarElement.style.display = "none";
+      dayCalendarElement.style.display = "flex";
     }
   });
 }
