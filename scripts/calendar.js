c1d333825d7cf70755842ec9c781ae9309d64134
Mateusz Ziomek <mateusz@mateuszziomekit.com>
2024-09-11 09:39:21 +0200

    part 7: week calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,13 +1,19 @@
 export function initCalendar() {
   const monthCalendarElement = document.querySelector("[data-month-calendar]");
+  const weekCalendarElement = document.querySelector("[data-week-calendar]");
 
   document.addEventListener("view-change", (event) => {
     const selectedView = event.detail.view;
 
     if (selectedView === "month") {
       monthCalendarElement.style.display = "flex";
+      weekCalendarElement.style.display = "none";
+    } else if (selectedView === "week") {
+      monthCalendarElement.style.display = "none";
+      weekCalendarElement.style.display = "flex";
     } else {
       monthCalendarElement.style.display = "none";
+      weekCalendarElement.style.display = "none";
     }
   });
 }
